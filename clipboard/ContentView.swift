9fcc41c2e254414ad3e4788b4644c5228aaf8741
wilay <ayomikunakintade@gmail.com>
2022-09-09 23:12:19 +0100

    macOS Clipboard

---
+++ clipboard/ContentView.swift
@@ -0,0 +1,138 @@
+//
+//  ContentView.swift
+//  clipboard
+//
+//  Created by Ayomikun Akintade on 09/09/2022.
+//
+
+
+import SwiftUI
+
+class ClipBoardViewModel : ObservableObject {
+    @Published var clipData:[ClipboardData] = []
+    var did = 0
+    
+    
+    func fetchClipboard() /*async*/ {
+        
+    }
+    
+    func addData(s: String, type: Int){
+        
+        self.clipData.insert(ClipboardData(text: s, type: type), at: 0)
+        self.did += 1
+    }
+    
+    
+}
+
+struct ClipboardData: Equatable {
+    var text: String
+    var type: Int
+}
+
+
+struct ContentView: View {
+    @EnvironmentObject private var vm : ClipBoardViewModel
+    var body: some View {
+        VStack(alignment:.leading, spacing: 1) {
+            HStack {
+                VStack (alignment:.leading, spacing: 3){
+                    Text("Clipboard")
+                        .font(.system(size: 15))
+                    Text("With ❤️ by Williams")
+                        .font(.system(size: 9))
+                        .foregroundColor(.green)
+                }
+                    .padding(8)
+                Spacer()
+                Image(systemName: "gear")
+                    .padding(.horizontal)
+                    .onTapGesture {
+                    }
+            }
+            .background(Color("555"))
+            ScrollView(.vertical, showsIndicators: false){
+                VStack {
+                    if vm.clipData.count > 0 {
+                        ForEach(vm.clipData, id: \.text){
+                            data in
+                            HStack (alignment:.center){
+                                Image(systemName: "circle.fill")
+                                    .font(.system(size: 5))
+                                if data.type == 0 {
+                                    Text(data.text)
+                                        .lineLimit(3)
+                                        .font(.system(size: 12))
+                                }
+                                else if data.type == 1 {
+                                    Text("")
+                                }
+                                Spacer()
+                                Image(systemName: /*data.active ? "checkmark" :*/ "doc.on.doc")
+                                    .foregroundColor(/*data.active ? .green :*/ .white)
+                                    .font(.system(size: 11))
+                                    .onTapGesture {
+                                        NSPasteboard.general.clearContents()
+                                        NSPasteboard.general.setString(data.text, forType: .string)
+                                        withAnimation {
+                                            //data.active = true
+                                        }
+                                        // Read copied string
+                                        //NSPasteboard.general.string(forType: .string)
+                                    }
+                                Image(systemName: /*data.active ? "checkmark" :*/ "multiply.circle")
+                                    .foregroundColor(/*data.active ? .green :*/ .white)
+                                    .font(.system(size: 11))
+                                    .onTapGesture {
+                                        if let index = vm.clipData.firstIndex(of: ClipboardData(text: data.text, type: data.type)) {
+                                            vm.clipData.remove(at: index)
+                                        }
+                                        
+                                    }
+                            }
+                            Divider()
+                        }
+                    }
+                    else {
+                        HStack {
+                            Spacer()
+                            Text("Nothing in Clipboard!")
+                            Spacer()
+                        }
+                        
+                    }
+                }
+                .padding()
+            }
+            .task {
+                vm.fetchClipboard()
+            }
+            
+            /*List(vm.clipData, id: \.text){ data in
+                HStack {
+                    Text("Hello My name is akintade ayomikun williams akintade and I am a")
+                        .lineLimit(3)
+                        .font(.system(size: 11))
+                    Spacer()
+                    Image(systemName: "doc.on.doc")
+                }
+                Divider()
+                    .padding([.vertical], 7)
+            }
+            .task {
+                vm.fetchClipboard()
+            }*/
+            
+            
+        }
+        .frame(width: 350, height: 350)
+        .background(.black)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ clipboard/clipboardApp.swift
@@ -0,0 +1,95 @@
+//
+//  clipboardApp.swift
+//  clipboard
+//
+//  Created by Ayomikun Akintade on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct clipboardApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    //@StateObject var vm = ClipBoardViewModel()
+    var body: some Scene {
+        Settings {
+            AnyView(EmptyView())
+        }
+        /*WindowGroup {
+            ContentView()
+                .environmentObject(vm)
+        }*/
+    }
+}
+
+extension NSNotification.Name {
+    public static let NSPasteboardDidChange: NSNotification.Name = .init(rawValue: "pasteboardDidChangeNotification")
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
+    
+    private var statusItems: NSStatusItem!
+    private var popover: NSPopover!
+    var timer: Timer!
+    let pasteboard: NSPasteboard = .general
+    var lastChangeCount: Int = 0
+    var vm = ClipBoardViewModel()
+    
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+
+        statusItems = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let statusButtton = statusItems.button {
+            statusButtton.image = NSImage(systemSymbolName: "doc.on.clipboard", accessibilityDescription: "Clipboard")
+            statusButtton.action = #selector(clipboardClicked)
+        }
+        
+        self.popover = NSPopover()
+        self.popover.contentSize = NSSize(width: 350, height: 350)
+        self.popover.behavior = .transient
+        self.popover.contentViewController = NSHostingController(rootView: ContentView().environmentObject(vm))
+        
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { (t) in
+                    if self.lastChangeCount != self.pasteboard.changeCount {
+                        self.lastChangeCount = self.pasteboard.changeCount
+                        NotificationCenter.default.post(name: .NSPasteboardDidChange, object: self.pasteboard)
+                    }
+                }
+        NotificationCenter.default.addObserver(self, selector: #selector(onPasteboardChanged), name: .NSPasteboardDidChange, object: nil)
+        
+    }
+    
+    @objc func clipboardClicked() {
+        
+        //update data Task {}
+        
+        
+        if let button = statusItems.button {
+            
+            if popover.isShown {
+                self.popover.performClose(nil)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+            
+        }
+        
+    }
+    
+    @objc
+        func onPasteboardChanged(_ notification: Notification) {
+            guard let pb = notification.object as? NSPasteboard else { return }
+            guard let items = pb.pasteboardItems else { return }
+            guard let item = items.first?.string(forType: .string) else { return }
+            
+            //guard let imgData = pb.data(forType: type) else { return }
+            
+            vm.addData(s: item, type: 0)
+            //print("New item in pasteboard: '\(item)'")
+            
+            
+        }
+    
+}
