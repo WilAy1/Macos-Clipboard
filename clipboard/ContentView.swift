d24da4d491319e2677569003ca52391c74658729
wilay <ayomikunakintade@gmail.com>
2022-09-10 22:29:51 +0100

    Added clear all and checkmark for copied item. Preventing multiple instances of same item and placing new items on top

--- clipboard/ContentView.swift
+++ clipboard/ContentView.swift
@@ -18,7 +18,9 @@ class ClipBoardViewModel : ObservableObject {
     }
     
     func addData(s: String, type: Int){
-        
+        if let index = self.clipData.firstIndex(of: ClipboardData(text: s, type: 0)) {
+            self.clipData.remove(at: index)
+        }
         self.clipData.insert(ClipboardData(text: s, type: type), at: 0)
         self.did += 1
     }
@@ -46,6 +48,13 @@ struct ContentView: View {
                 }
                     .padding(8)
                 Spacer()
+                Text("CLEAR")
+                    .font(.system(size: 13))
+                    .fontWeight(.bold)
+                    .foregroundColor(.red)
+                    .onTapGesture {
+                        vm.clipData.removeAll()
+                    }
                 Image(systemName: "rectangle.portrait.and.arrow.right")
                     .padding(.horizontal)
                     .onTapGesture {
@@ -58,41 +67,8 @@ struct ContentView: View {
                     if vm.clipData.count > 0 {
                         ForEach(vm.clipData, id: \.text){
                             data in
-                            HStack (alignment:.center){
-                                Image(systemName: "circle.fill")
-                                    .font(.system(size: 5))
-                                if data.type == 0 {
-                                    Text(data.text)
-                                        .lineLimit(3)
-                                        .font(.system(size: 12))
-                                }
-                                else if data.type == 1 {
-                                    Text("")
-                                }
-                                Spacer()
-                                Image(systemName: /*data.active ? "checkmark" :*/ "doc.on.doc")
-                                    .foregroundColor(/*data.active ? .green :*/ .white)
-                                    .font(.system(size: 11))
-                                    .onTapGesture {
-                                        NSPasteboard.general.clearContents()
-                                        NSPasteboard.general.setString(data.text, forType: .string)
-                                        withAnimation {
-                                            //data.active = true
-                                        }
-                                        // Read copied string
-                                        //NSPasteboard.general.string(forType: .string)
-                                    }
-                                Image(systemName: /*data.active ? "checkmark" :*/ "multiply.circle")
-                                    .foregroundColor(/*data.active ? .green :*/ .white)
-                                    .font(.system(size: 11))
-                                    .onTapGesture {
-                                        if let index = vm.clipData.firstIndex(of: ClipboardData(text: data.text, type: data.type)) {
-                                            vm.clipData.remove(at: index)
-                                        }
-                                        
-                                    }
-                            }
-                            Divider()
+                            eachCopiedItem(data: data)
+                                .environmentObject(vm)
                         }
                     }
                     else {
@@ -129,6 +105,53 @@ struct ContentView: View {
         }
         .frame(width: 350, height: 350)
         .background(.black)
+    }
+}
+
+struct eachCopiedItem: View {
+    @State var data: ClipboardData
+    @State var copied = false
+    @EnvironmentObject private var vm: ClipBoardViewModel
+    var body: some View {
+        HStack (alignment:.center){
+            Image(systemName: "circle.fill")
+                .font(.system(size: 5))
+            if data.type == 0 {
+                Text(data.text)
+                    .lineLimit(3)
+                    .font(.system(size: 12))
+            }
+            else if data.type == 1 {
+                Text("")
+            }
+            Spacer()
+            Image(systemName: copied ? "checkmark" : "doc.on.doc")
+                .foregroundColor(copied ? .green : .white)
+                .font(copied ? .system(size: 13) : .system(size: 11))
+                .onTapGesture {
+                    NSPasteboard.general.clearContents()
+                    NSPasteboard.general.setString(data.text, forType: .string)
+                    withAnimation {
+                        copied = true
+                        //data.active = true
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                        copied = false
+                    }
+                    // Read copied string
+                    //NSPasteboard.general.string(forType: .string)
+                }
+            Image(systemName: /*data.active ? "checkmark" :*/ "multiply.circle")
+                .foregroundColor(/*data.active ? .green :*/ .white)
+                .font(.system(size: 11))
+                .onTapGesture {
+                    if let index = vm.clipData.firstIndex(of: ClipboardData(text: data.text, type: data.type)) {
+                        vm.clipData.remove(at: index)
+                    }
+                    
+                }
+        }
+        Divider()
     }
 }
 
